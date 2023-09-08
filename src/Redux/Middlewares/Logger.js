63a2e646dfc7017a43bf85bb2c29e96adb9f9200
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-09-09 00:17:25 +0600

    created a custom logger

--- src/Redux/Middlewares/Logger.js
+++ src/Redux/Middlewares/Logger.js
@@ -1,8 +1,9 @@
 const logger = store => next => action => {
     const currentState = store.getState();
-    console.log(currentState);
-    let result = next(action);
-    return result
+    console.log("Current state",currentState);
+    console.log("Action dispatched", action);
+    next(action);
+    console.log("Updated state", store.getState());    
 }
 
 export default logger;
